2133bd33cc2a3ab65d218d61cdd3e089add43c8b
Vimal Thanikachalam <vimaleurakaa@gmail.com>
2021-10-19 18:40:01 +0530

    server-cors-settings-updated

--- server/app.js
+++ server/app.js
@@ -2,15 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
-
+const dotenv = require("dotenv");
 const routes = require("./routes/index");
 
-app.use(cors());
+dotenv.config();
+
+const whitelist = process.env.WHITELIST_URL;
+const corsConfig = {
+  origin: function (origin = process.env.PUBLIC_URL, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsConfig));
 app.use(express.json());
 app.use("/images", express.static("images"));
 app.use(routes);
 app.use(morgan("dev"));
 
-app.listen(5000, () => {
-  console.log("Server Started...");
+app.listen(process.env.PORT, () => {
+  console.log("MediAssist Node Server Started...");
 });
